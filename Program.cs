64abd07cf502ad71b55472d5fa3ace71092e23df
Muhamad Adri Muwaffaq Khamid <muhamad.adri.ft21@mail.umy.ac.id>
2022-12-28 17:06:42 +0700

    declare variable and make node class

--- Program.cs
+++ Program.cs
@@ -1,11 +1,18 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection.Emit;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace BinarySearchTree
 {
+    class Node
+    {
+        public string info;
+        public string leftchild;
+        public string rightchild;
+    }
     internal class Program
     {
         static void Main(string[] args)
